5437184d091fbcdc075844818a5c0ac3d54e1f6c
xmdhs <xmdhss@gmail.com>
2022-06-18 23:28:08 +0800

    fix

--- src/components/danmakuList.vue
+++ src/components/danmakuList.vue
@@ -135,10 +135,6 @@ async function handleSelect(e: string) {
             filtered.value = true
             break;
         case 'blocked':
-            tempDmList = data.value.filter(() => true)
-            data.value = tempDmList.filter(dm => {
-                return dm.sendID != selectDm.sendID
-            })
             emit('block', selectDm.sendID)
             break;
 
