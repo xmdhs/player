81fd93ef6571306092cb56a2764cb07bb5414938
xmdhs <xmdhss@gmail.com>
2022-07-20 01:09:06 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { join } from "path";
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: "> 0.2%, last 2 versions, Firefox ESR, not dead"
+    })
+  ],
   build: {
     sourcemap: true,
   },
